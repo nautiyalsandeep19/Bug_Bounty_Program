3606947dd925cbbe60a26d40929d7fca25de4765
SANDEEP NAUTIYAL <138180022+nautiyalsandeep19@users.noreply.github.com>
2025-05-06 11:50:27 +0530

    added store

--- server/Models/company.js
+++ server/Models/company.js
@@ -32,16 +32,16 @@ const companySchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   domain: { type: String, required: true },
-  // createdAt: { type: Date, default: Date.now },
-  // description: { type: String },
-  // programs: { type: mongoose.Schema.Types.ObjectId, ref: "Program" },
-  // invitedHackers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Hacker" }],
-  // contactPerson: {
-  //   representative: { type: String },
-  //   position: { type: String },
-  //   phone: { type: String }
-  // },
-  // profileImage: { type: String }
+  createdAt: { type: Date, default: Date.now },
+  description: { type: String },
+  programs: { type: mongoose.Schema.Types.ObjectId, ref: "Program" },
+  invitedHackers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Hacker" }],
+  contactPerson: {
+    representative: { type: String },
+    position: { type: String },
+    phone: { type: String }
+  },
+  profileImage: { type: String }
 });
 
 const Company = mongoose.model('Company', companySchema);
