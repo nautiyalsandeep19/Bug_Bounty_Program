6393d9deb288d2e3d318f42c3bde5d7dfaa4eaad
01TUSHAR <01tusharpal@gmail.com>
2025-05-10 18:01:03 +0530

    updated code

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -13,7 +13,7 @@ import {
 const Sidebar = () => {
   return (
     <div className="w-64 bg-[#121212] p-4 hidden sm:block">
-      <h1 className="text-green-500 text-2xl font-bold mb-8">Versantix</h1>
+      <h1 className="text-blue-500 text-3xl font-bold mb-8">Versantix</h1>
       <nav className="flex flex-col gap-4">
         <NavLink
           to="/hacker/dashboard"
