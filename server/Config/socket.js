dd22a68ddd3ed869750ff8de78560e68cf58d889
01TUSHAR <01tusharpal@gmail.com>
2025-06-03 16:40:51 +0530

    updated code

--- server/Config/socket.js
+++ server/Config/socket.js
@@ -1,32 +1,3 @@
-// import express from 'express'
-// import http from 'http'
-// import { Server } from 'socket.io'
-
-// const app = express()
-// const server = http.createServer(app)
-
-// // Allow CORS from your frontend origin
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["http://localhost:5173"], // Replace with your frontend URL
-//     methods: ["GET", "POST"]
-//   }
-// });
-
-
-// io.on('connection', (socket) => {
-//   console.log('A user connected:', socket.id);
-
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected:', socket.id);
-//   });
-// });
-
-
-// export { app ,io , server}
-
-
-// ./Config/socket.js
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
