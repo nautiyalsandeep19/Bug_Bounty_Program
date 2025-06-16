ce546adab43a43a3000b72eaad5d121ec3693113
01TUSHAR <01tusharpal@gmail.com>
2025-06-16 12:40:45 +0530

    updated Code

--- client/src/Common/LayoutSidebar/CommonLayout.jsx
+++ client/src/Common/LayoutSidebar/CommonLayout.jsx
@@ -3,9 +3,11 @@ import CommonSidebar from './CommonSideBar'
 
 const CommonLayout = () => {
   return (
-    <div className="flex">
+    <div className="flex  bg-[#0e0e0e] text-white ">
+      {/* Sidebar stays fixed */}
       <CommonSidebar />
-      <div className="w-full">
+      {/* Main content scrolls */}
+      <div className="flex-1 min-h-screen overflow-y-auto p-4">
         <Outlet />
       </div>
     </div>
