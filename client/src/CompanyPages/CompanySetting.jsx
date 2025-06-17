a030c57c4acd143697fe7562a40457f2262ff06e
sumitkainturaji <kainturasumit190@gmail.com>
2025-06-17 13:00:03 +0530

    working on the company setting controller and frontend

--- client/src/CompanyPages/CompanySetting.jsx
+++ client/src/CompanyPages/CompanySetting.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import CTAButton from '../Common/Button/CTAButton'
 import { updateCompanyProfile } from '../Services/companyApi'
 import { useDispatch, useSelector } from 'react-redux'
